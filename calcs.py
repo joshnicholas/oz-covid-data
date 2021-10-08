ca35891a6b14ce3336ec03a151ea589c72fc974d
Josh Nicholas <hi@thambi.li>
2021-10-08 16:01:19 +1100

    New calcs

---
+++ calcs.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import os 
+
+
+listo = []
+
+for file in os.listdir('data'):
+    # print(file)
+    if "aged_care_home" in file:
+        inter = pd.read_csv(f'data/{file}', parse_dates=['Date'])
+        print(inter)
+        # print(file)
+        listo.append(inter)
+
+
+final = pd.concat(listo)
+final = final.sort_values(by='Date', ascending=True)
+
+final.drop_duplicates()
+
+print(final)
