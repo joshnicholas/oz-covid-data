bde36bb5f2a1626d25bfc30ddaf7b354e4a56975
Josh Nicholas <hi@thambi.li>
2021-12-24 07:42:52 +0530

    Removed OECD chart from scraper

--- archive/health_scrape_test.py
+++ archive/health_scrape_test.py
@@ -12,7 +12,7 @@ today = utc_now.astimezone(pytz.timezone("Australia/Brisbane"))
 today = today.strftime('%Y-%m-%d')
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 
 driver = webdriver.Firefox(options=chrome_options)
 
@@ -22,8 +22,8 @@ start_url = 'https://www.health.gov.au/news/health-alerts/novel-coronavirus-2019
 
 #%%
 driver.get(start_url)
-driver.implicitly_wait(15)
-time.sleep(15)
+# driver.implicitly_wait(35)
+time.sleep(10)
 
 #%%
 
@@ -33,6 +33,7 @@ i = 0
 
 ## Need to wait while all the tables load
 while len(tables) < 9:
+    print(i, len(tables))
     # driver.implicitly_wait(10)
     time.sleep(1)
     tables = pd.read_html(driver.page_source.encode("utf-8"))
@@ -40,6 +41,7 @@ while len(tables) < 9:
 
     if i > 600:
         print("Break")
+        driver.close()
         break
 
 names = {0:"recent_cases", 1:"total_cases", 2:"cases_age_sex",
@@ -57,24 +59,27 @@ if len(tables) >= 9:
         table['Date'] = today
         title = f"{today}_{names[i]}"
 
+        print(table)
+
         ## Dump individual file
 
-        with open(f"data/{title}.csv", "w") as f:
-            table.to_csv(f, index=False, header=True)
+        # with open(f"data/{title}.csv", "w") as f:
+        #     table.to_csv(f, index=False, header=True)
 
-        ## Add data to the output files
+        # ## Add data to the output files
 
-        old = pd.read_csv(f'output/{names[i]}.csv')
-        # print(names[i])
-        # print(old.shape)
-        combo = old.append(table)
-        # print(combo.shape)
-        combo = combo.drop_duplicates(keep='last')
-        # print("Dropped", combo.shape)
+        # old = pd.read_csv(f'output/{names[i]}.csv')
+        # # print(names[i])
+        # # print(old.shape)
+        # combo = old.append(table)
+        # # print(combo.shape)
+        # combo = combo.drop_duplicates(keep='last')
+        # # print("Dropped", combo.shape)
 
-        with open(f'output/{names[i]}.csv', 'w') as f:
-            combo.to_csv(f, index=False, header=True)
+        # with open(f'output/{names[i]}.csv', 'w') as f:
+        #     combo.to_csv(f, index=False, header=True)
 
 
         i += 1
 
+# driver.close()
