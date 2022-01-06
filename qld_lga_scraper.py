4d097a312152bedeafa64b3e24115cc059c0a3d2
Josh Nicholas <hi@thambi.li>
2022-01-06 05:53:03 +0530

    Redo

--- qld_lga_scraper.py
+++ qld_lga_scraper.py
@@ -23,44 +23,44 @@ with open(f'qld_lga_data/{today}_qld_lga.csv', 'w') as f:
     table.to_csv(f, index=False, header=True)
 
 
-old = pd.read_csv('output/qld_lgas.csv')
+# old = pd.read_csv('output/qld_lgas.csv')
 
-final = old.append(table)
+# final = old.append(table)
 
-final = final.drop_duplicates(subset=['Local Government Area', 'Date'], keep='last')
-final = final[['Local Government Area', 'Total', 'Date']]
-final.columns = ["LGA", 'Total cases', 'Date']
-with open('output/qld_lgas.csv', 'w') as f:
-    final.to_csv(f, index=False, header=True)
+# final = final.drop_duplicates(subset=['Local Government Area', 'Date'], keep='last')
+# final = final[['Local Government Area', 'Total', 'Date']]
+# final.columns = ["LGA", 'Total cases', 'Date']
+# with open('output/qld_lgas.csv', 'w') as f:
+#     final.to_csv(f, index=False, header=True)
 
-p = final
+# p = final
 
-print(p)
-print(p.columns)
+# print(p)
+# print(p.columns)
 
 # # Process into one file
 
-# listo = []
+listo = []
 
-# for file in os.listdir('qld_lga_data'):
+for file in os.listdir('qld_lga_data'):
 
-#     inter = pd.read_csv(f'qld_lga_data/{file}', parse_dates=['Date'])
-#     inter = inter[['Local Government Area', 'Total', 'Date']]
-#     inter.columns = ["LGA", 'Total cases', 'Date']
-#     # print(inter)
-#     # # print(file)
-#     inter = inter.loc[inter['LGA'] !="Total"]
+    inter = pd.read_csv(f'qld_lga_data/{file}', parse_dates=['Date'])
+    inter = inter[['Local Government Area', 'Total', 'Date']]
+    inter.columns = ["LGA", 'Total cases', 'Date']
+    # print(inter)
+    # # print(file)
+    inter = inter.loc[inter['LGA'] !="Total"]
 
 
-#     listo.append(inter)
-#     # print(inter.columns.tolist())
+    listo.append(inter)
+    # print(inter.columns.tolist())
 
-# qld = pd.concat(listo)
-# qld = qld.sort_values(by='Date', ascending=True)
+qld = pd.concat(listo)
+qld = qld.sort_values(by='Date', ascending=True)
 
-# qld['Date'] = pd.to_datetime(qld['Date'])
+qld['Date'] = pd.to_datetime(qld['Date'])
 
-# print(qld)
+print(qld)
 
-# with open('output/qld_lgas.csv', 'w') as f:
-#     qld.to_csv(f, index=False, header=True)
+with open('output/qld_lgas.csv', 'w') as f:
+    qld.to_csv(f, index=False, header=True)
